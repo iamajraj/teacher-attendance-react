f3dd69b0986f44ea650fd6a567a2646daac941f9
iamajraj <akmalraj07@gmail.com>
2023-03-28 19:26:44 +0600

    teacher delete added

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,48 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Space, Table } from "antd";
 import { Link } from "react-router-dom";
+import { Modal } from "antd";
+import { ExclamationCircleFilled } from "@ant-design/icons";
 
-const Teachers = () => {
-    return (
-        <div className="mt-10">
-            <TeacherTable />
-        </div>
-    );
-};
-
-export default Teachers;
-
-const columns = [
-    {
-        title: "Name",
-        dataIndex: "name",
-        key: "name",
-    },
-    {
-        title: "Username",
-        dataIndex: "username",
-        key: "username",
-    },
-    {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
-    },
-    {
-        title: "Action",
-        key: "action",
-        render: (_, record) => (
-            <Space size="middle">
-                <Link to={`/attendance/${record.id}`} state={record}>
-                    View
-                </Link>
-                <a className="text-red-500">Delete</a>
-            </Space>
-        ),
-    },
-];
-
-const data = [
+const teachers = [
     {
         id: "1",
         name: "John Brown",
@@ -51,16 +13,85 @@ const data = [
     },
     {
         id: "2",
-        name: "John Brown",
-        username: "john_brown",
-        email: "john@gmail.com",
+        name: "Alex Smith",
+        username: "alex_smith",
+        email: "alex@gmail.com",
     },
     {
         id: "3",
-        name: "John Brown",
-        username: "john_brown",
-        email: "john@gmail.com",
+        name: "James Bond",
+        username: "james_bond",
+        email: "james@gmail.com",
     },
 ];
 
-const TeacherTable = () => <Table columns={columns} dataSource={data} />;
+const Teachers = () => {
+    const [data, setData] = useState(teachers);
+
+    const removeTeacher = (id) => {
+        setData((prev) => prev.filter((t) => t.id !== id));
+    };
+
+    return (
+        <div className="mt-10">
+            <TeacherTable data={data} removeTeacher={removeTeacher} />
+        </div>
+    );
+};
+
+export default Teachers;
+
+const TeacherTable = ({ data, removeTeacher }) => {
+    const { confirm } = Modal;
+
+    const showDeleteConfirm = (id) => {
+        confirm({
+            title: "Are you sure delete this teacher?",
+            icon: <ExclamationCircleFilled />,
+            okText: "Yes",
+            okType: "danger",
+            cancelText: "No",
+            onOk() {
+                removeTeacher(id);
+            },
+        });
+    };
+
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Username",
+            dataIndex: "username",
+            key: "username",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email",
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (_, record) => (
+                <Space size="middle">
+                    <Link to={`/attendance/${record.id}`} state={record}>
+                        View
+                    </Link>
+                    <a
+                        className="text-red-500"
+                        onClick={() => {
+                            showDeleteConfirm(record.id);
+                        }}
+                    >
+                        Delete
+                    </a>
+                </Space>
+            ),
+        },
+    ];
+    return <Table columns={columns} dataSource={data} />;
+};
